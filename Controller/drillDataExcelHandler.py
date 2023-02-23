9e6d79d0422c6bbe953e343f23a9367455708886
Jackie Liu <we1019000306@163.com>
2023-02-23 17:28:22 +0800

    save data into mongodb

--- Controller/drillDataExcelHandler.py
+++ Controller/drillDataExcelHandler.py
@@ -7,12 +7,13 @@ import xlwt
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QThread, pyqtSignal, QMutex, Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QFileDialog, QApplication, QTableWidgetItem, QHeaderView
-
+from PyQt5.QtWidgets import QFileDialog, QApplication, QTableWidgetItem, QHeaderView, QMessageBox
 from View.ReaderBaseInfoUI import Ui_MainWindow
+import pymongo
+
 globalAllInfoList:list = []
 globalFilesPathList:list = []
-
+globalCollectionName:list = []
 class window(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
         super().__init__()
@@ -21,6 +22,8 @@ class window(QtWidgets.QMainWindow,Ui_MainWindow):
         self.selectFileButton.clicked.connect(self.loadBaseData)
         self.selectFileButton.clicked.connect(self.setTableViewWithData)
         self.savePushButton.clicked.connect(self.saveBtnClicked)
+        self.savePushButton.clicked.connect(self.savedInMongoDB)
+
 
     def getFileOnClicked(self):
         global globalFilesPathList
@@ -47,11 +50,25 @@ class window(QtWidgets.QMainWindow,Ui_MainWindow):
 
     def loadBaseData(self):
         global globalFilesPathList
-
+        global globalCollectionName
         if len(globalFilesPathList) > 0:
             for i in globalFilesPathList:
                 #print(i)
-                loadDataFromExcel(i)
+                patternName = re.compile(r'[0-9]+月+[0-9]+日')
+                if patternName.search(i):
+                    globalCollectionName.append(patternName.search(i).group())
+                else:
+                    QMessageBox.information(MainWindow,
+                                            '警告！！！',
+                                            '请确认文件名中是否存在日期信息')
+                if len(set(globalCollectionName)) == 1:
+                    loadDataFromExcel(i)
+                    print(set(globalCollectionName))
+                else:
+                    QMessageBox.information(MainWindow,
+                                            '警告！！！',
+                                            '请确认所选生产日报是否为同一天！！！')
+
         else:
             print('未导入文件！！！！')
 
@@ -122,9 +139,34 @@ class window(QtWidgets.QMainWindow,Ui_MainWindow):
 
         Excel.save(r'C:\Users\18637\Desktop\院属钻机生产日报.xlsx')
 
+    def savedInMongoDB(self):
+        global globalAllInfoList
+        client = pymongo.MongoClient(host='localhost', port=27017)
+        db = client.drillProject
+        collection = db.drillProjectItems
+        rowCount = self.dataTableWidget.rowCount()
+        # columnCount = self.dataTableWidget.columnCount()
+        i = 0
+        while i < rowCount:
+            # j = 0
+            # while j < columnCount:
+            #     # drillProjectItem = ['company':]
+            #     j = j + 1
+            keysList = ['company','projectName','drillId','currentDeep','lastDayDeep','workState']
+            print(globalAllInfoList[i])
+            projectItem = []
+            for infoList in globalAllInfoList[i]:
+                infoListStr = ''.join(infoList)
+                projectItem.append(infoListStr)
+                print(projectItem)
+            drillProjectItem =  dict(zip(keysList,projectItem))
+            print(drillProjectItem)
+            result = collection.insert_one(drillProjectItem)
+            print(result)
+            i = i + 1
+
 def loadDataFromExcel(fileNames: str):
     global globalAllInfoList
-
     path_openfile_name = fileNames
 
     if path_openfile_name != '':
@@ -145,86 +187,104 @@ def loadDataFromExcel(fileNames: str):
         workingStateList = []
         tipsList = []
         m = 0
-        for i in dataList:
-            # 索引出每个不为空的第一行即为新的项目数据行
-            if str(i[0]) != 'nan':
-                drillInfoList.clear()
-                companyList.clear()
-                drillProjectNameList.clear()
-                drillNumList.clear()
-                deepList.clear()
-                perDayDeepList.clear()
-                workingStateList.clear()
-                tipsList.clear()
+        if 0 < len(dataList):
+            for i in dataList:
+                # 索引出每个不为空的第一行即为新的项目数据行
+                if str(i[0]) != 'nan':
+                    drillInfoList.clear()
+                    companyList.clear()
+                    drillProjectNameList.clear()
+                    drillNumList.clear()
+                    deepList.clear()
+                    perDayDeepList.clear()
+                    workingStateList.clear()
+                    tipsList.clear()
 
-                companyList.append(str(i[0]))
-                drillInfoStrList = str(i[1]).split()
-                drillInfoStr = str(drillInfoStrList)
-                drillNameStr = str(i[1]).split()[0]
-                # 正则表达找出是项目名称
-                # patternName = re.compile(r'^[\u4e00-\u9fa5]+')
-                # if patternName.search(drillInfoStr):
-                #     drillNameStr = patternName.search(drillInfoStr).group()
-                # else:
-                #     drillNameStr = 'xxxx'
-                #     print('未找到项目名称！！！')
-                drillProjectNameList.append(drillNameStr)
-                print(drillNameStr)
-                # 正则表达找出是否为队属钻机
-                patternNum = re.compile(r'[-[0-9]+[\u4E00-\u9FA5A-Za-z0-9]+（.*\属）')
-                patternNum1 = re.compile(r'[-[0-9]+[\u4E00-\u9FA5A-Za-z0-9]+（.*\协）')
-                patternNum2 = re.compile(r'[-[0-9]+[\u4E00-\u9FA5A-Za-z0-9]+（.*\管）')
+                    companyList.append(str(i[0]))
+                    drillInfoStrList = str(i[1]).split()
+                    drillInfoStr = str(drillInfoStrList)
+                    drillNameStr = str(i[1]).split()[0]
+                    # 正则表达找出是项目名称
+                    # patternName = re.compile(r'^[\u4e00-\u9fa5]+')
+                    # if patternName.search(drillInfoStr):
+                    #     drillNameStr = patternName.search(drillInfoStr).group()
+                    # else:
+                    #     drillNameStr = 'xxxx'
+                    #     print('未找到项目名称！！！')
+                    drillProjectNameList.append(drillNameStr)
+                    print(drillNameStr)
+                    # 正则表达找出是否为队属钻机
+                    patternNum = re.compile(r'[-[0-9]+[\u4E00-\u9FA5A-Za-z0-9]+（.*\属）')
+                    patternNum1 = re.compile(r'[-[0-9]+[\u4E00-\u9FA5A-Za-z0-9]+（.*\协）')
+                    patternNum2 = re.compile(r'[-[0-9]+[\u4E00-\u9FA5A-Za-z0-9]+（.*\管）')
 
-                if patternNum.search(drillInfoStr):
-                    drillNumStr = patternNum.search(drillInfoStr).group()
-                else:
-                    if patternNum1.search(drillInfoStr):
-                        drillNumStr = patternNum1.search(drillInfoStr).group()
+                    if patternNum.search(drillInfoStr):
+                        drillNumStr = patternNum.search(drillInfoStr).group()
                     else:
-                        if patternNum2.search(drillInfoStr):
-                            drillNumStr = patternNum2.search(drillInfoStr).group()
+                        if patternNum1.search(drillInfoStr):
+                            drillNumStr = patternNum1.search(drillInfoStr).group()
                         else:
-                            drillNumStr = 'xxxx'
-                            print('未匹配！！！！')
-                print(drillNumStr)
-                if '队管' in drillNumStr:
-                    drillNumStr = drillNumStr.replace('管', '属')
-                drillNumList.append(drillNumStr)
-                # 项目名称+施工地点+钻机编号+孔号+设计孔深+井型+孔径+开孔日期
-                drillInfoList.append(
-                    ''.join(drillInfoStrList[2] + '\n' + drillInfoStrList[1] + '\n' + drillInfoStrList[0]))
+                            if patternNum2.search(drillInfoStr):
+                                drillNumStr = patternNum2.search(drillInfoStr).group()
+                            else:
+                                drillNumStr = 'xxxx'
+                                print('未匹配！！！！')
+                    print(drillNumStr)
+                    if '队管' in drillNumStr:
+                        drillNumStr = drillNumStr.replace('管', '属')
+                    drillNumList.append(drillNumStr)
+                    # 项目名称+施工地点+钻机编号+孔号+设计孔深+井型+孔径+开孔日期
+                    if len(drillInfoStrList)>3:
+                        drillInfoList.append(
+                            ''.join(drillInfoStrList[2] + '\n' + drillInfoStrList[1] + '\n' + drillInfoStrList[0]))
+                    else:
+                        QMessageBox.information(MainWindow,
+                                                '警告！！',
+                                                '数据源不合法！！请选择生产日报！！')
+                        break
+                    # print('钻孔深度：' + str(input_table.iloc[n, 2]) + '(m)')
+                    deepList.append(str(input_table.iloc[m + 3, 2]) + '(m)')
 
-                # print('钻孔深度：' + str(input_table.iloc[n, 2]) + '(m)')
-                deepList.append(str(input_table.iloc[m + 3, 2]) + '(m)')
+                    # print('日进尺：' + str(input_table.iloc[m, 3]) + '(m)')
+                    perDayDeepList.append(str(input_table.iloc[m + 3, 3]) + '(m)')
 
-                # print('日进尺：' + str(input_table.iloc[m, 3]) + '(m)')
-                perDayDeepList.append(str(input_table.iloc[m + 3, 3]) + '(m)')
+                    # print('工况：' + str(input_table.iloc[m, 5]))
+                    workingStateList.append('6:00-10:00' + ''.join(str(input_table.iloc[m + 3, 5]).split()))
 
-                # print('工况：' + str(input_table.iloc[m, 5]))
-                workingStateList.append('6:00-10:00' + ''.join(str(input_table.iloc[m + 3, 5]).split()))
+                    # print('备注：' + str(input_table.iloc[m, 16]))
+                    tipsList.append(str(input_table.iloc[m, 16]))
+                else:
+                    if m % 6 == 1:
+                        workingStateList.append('10:00-14:00' + ''.join(str(input_table.iloc[m, 5]).split()))
+                    elif m % 6 == 2:
+                        workingStateList.append('14:00-18:00' + ''.join(str(input_table.iloc[m, 5]).split()))
+                    elif m % 6 == 3:
+                        workingStateList.append('18:00-22:00' + ''.join(str(input_table.iloc[m, 5]).split()))
+                    elif m % 6 == 4:
+                        workingStateList.append('22:00-2:00' + ''.join(str(input_table.iloc[m, 5]).split()))
+                    elif m % 6 == 5:
+                        workingStateList.append('2:00-6:00' + ''.join(str(input_table.iloc[m, 5]).split()))
+                        ndList1 = [companyList.copy(), drillProjectNameList.copy(), drillNumList.copy(), deepList.copy(), perDayDeepList.copy(),
+                                workingStateList.copy(), tipsList.copy()]
+                        ndArray = np.array(ndList1, dtype='object')
 
-                # print('备注：' + str(input_table.iloc[m, 16]))
-                tipsList.append(str(input_table.iloc[m, 16]))
-            else:
-                if m % 6 == 1:
-                    workingStateList.append('10:00-14:00' + ''.join(str(input_table.iloc[m, 5]).split()))
-                elif m % 6 == 2:
-                    workingStateList.append('14:00-18:00' + ''.join(str(input_table.iloc[m, 5]).split()))
-                elif m % 6 == 3:
-                    workingStateList.append('18:00-22:00' + ''.join(str(input_table.iloc[m, 5]).split()))
-                elif m % 6 == 4:
-                    workingStateList.append('22:00-2:00' + ''.join(str(input_table.iloc[m, 5]).split()))
-                elif m % 6 == 5:
-                    workingStateList.append('2:00-6:00' + ''.join(str(input_table.iloc[m, 5]).split()))
-                    ndList1 = [companyList.copy(), drillProjectNameList.copy(), drillNumList.copy(), deepList.copy(), perDayDeepList.copy(),
-                            workingStateList.copy(), tipsList.copy()]
-                    ndArray = np.array(ndList1, dtype='object')
+                        if '外协' not in drillNumStr and '队属' in drillNumStr:
+                            globalAllInfoList.append(ndArray)
+                        else:
+                            print('数据不合法哦！！！')
 
-                    if '外协' not in drillNumStr:globalAllInfoList.append(ndArray)
-            m += 1
-        print(globalAllInfoList)
+                m += 1
+            print(globalAllInfoList)
+        else:
+            QMessageBox.information(MainWindow,
+                                    '警告！！',
+                                    '数据源不合法！！请选择生产日报！！')
 
-#
+    else:
+        QMessageBox.information(MainWindow,
+                                '警告！！',
+                                '你未选择任何文件！！')
+
 # def writeDataInExcel():
 #     print(111)
 
